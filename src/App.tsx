309f43f87dedd39afed4a383feee1d056cd7e123
octotoe <20903206+GalacticOctopus@users.noreply.github.com>
2023-03-17 19:38:09 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Home } from "./pages/Home";
 import "./scss/styles.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
