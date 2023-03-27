cfe727f8bed3ff5ea0bf4e02d68e282b33fe6a33
octotoe <20903206+GalacticOctopus@users.noreply.github.com>
2023-03-27 17:09:58 +0200

    firebase auth

---
+++ config/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+interface FbConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+
+const firebaseConfig: FbConfig = {
+  apiKey: "AIzaSyDQzY199cPs0wChs4Dr-hYdSEk5voOtr08",
+  authDomain: "scooter-reservation-bcda2.firebaseapp.com",
+  projectId: "scooter-reservation-bcda2",
+  storageBucket: "scooter-reservation-bcda2.appspot.com",
+  messagingSenderId: "34359124842",
+  appId: "1:34359124842:web:35cbc6fc8c7d8f57d8bb13",
+  measurementId: "G-NRGTE3FDLN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
