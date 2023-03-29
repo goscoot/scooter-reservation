0ee1323e963b92c5c6385f0cd831d616eabd505f
masterHAWK99 <maciej.pochron99@gmail.com>
2023-03-29 21:54:22 +0200

    Improve naming for classes

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,15 +4,15 @@ import FilterableProductList from "../components/products/FilterableProductList"
 const Products = () => {
   return (
     <>
-      <header className="products__header">
-        <div className="image__wrapper">
+      <header className="products-header">
+        <div className="products-header__image-wrapper">
           <img
-            className="header__image"
+            className="products-header__image"
             src={productsHeaderImage}
             alt="people riding on scooters"
           />
         </div>
-        <hgroup className="header__text">
+        <hgroup className="products-header__text">
           <h1 className="text-heading1 text-100">
             Our <span className="text-400">scooters</span>.
           </h1>
