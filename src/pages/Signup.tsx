508055a281b825bf2b275f7ebaf15d0c31b0f573
Maciej Zajączkowski <61516190+mzajaczkowsky@users.noreply.github.com>
2023-03-29 12:43:47 +0200

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,7 +3,6 @@ import { Register } from "./Register";
 export function Signup() {
   return (
     <div className="signup">
-      {/* navbar will be placed there */}
       <Register />
     </div>
   );
