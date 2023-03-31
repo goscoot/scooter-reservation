07806883227f1d267a3138e6ad0689131226149d
Maciej Zajączkowski <61516190+mzajaczkowsky@users.noreply.github.com>
2023-03-31 12:32:59 +0200

    Update Login.tsx
    additional changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,54 +1,75 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export function Login() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
-  const manageSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    console.log(`${email} | ${password}`);
-  };
-
   return (
     <div className="signin__login">
       <div className="signin__login__container">
-        <form onSubmit={manageSubmit}>
-          <div className="signin__wrapper1">
-            <h1 className="text-heading1">Login</h1>
-          </div>
-          <div className="signin__wrapper2">
-            {/* e-mail & password */}
-            <div className="signin__wrapper2__mail">
-              <div className="text-caption">E-mail</div>
-              <input
-                className="input-text"
-                type="text"
-                placeholder="John Doe"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Email is required"),
+            password: Yup.string()
+              .min(6, "Password must be at least 6 characters long")
+              .required("Password is required"),
+          })}
+          onSubmit={(values) => {
+            setTimeout(() => {
+              //fetch actions will be executed there
+            }, 400);
+          }}
+        >
+          <Form>
+            <div className="signin__wrapper1">
+              <h1 className="text-heading1">Login</h1>
             </div>
-            <div className="signin__wrapper2__pass">
-              <div className="text-caption">Password</div>
-              <input
-                className="input-text"
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+            <div className="signin__wrapper2">
+              <div className="signin__wrapper2__mail">
+                <div className="text-caption">E-mail</div>
+                <Field
+                  className="input-text"
+                  type="text"
+                  name="email"
+                  placeholder="johndoe@gmail.com"
+                />
+                <ErrorMessage
+                  className="error-message"
+                  name="email"
+                  component="div"
+                />
+              </div>
+              <div className="signin__wrapper2__pass">
+                <div className="text-caption">Password</div>
+                <Field
+                  className="input-text"
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                />
+                <ErrorMessage
+                  className="error-message"
+                  name="password"
+                  component="div"
+                />
+              </div>
             </div>
-          </div>
-          <div className="signin__wrapper3">
-            {/* confirm button & register */}
-            <input className="btn btn-primary" type="submit" />
-            <Link to="/register" className="signin__wrapper3__link">
-              <span className="link-underlined">Don’t have account yet? </span>
-              <span className="link-colored">Register</span>
-            </Link>
-          </div>
-        </form>
+            <div className="signin__wrapper3">
+              <button className="btn btn-primary" type="submit">
+                Login
+              </button>
+              <Link to="/register" className="signin__wrapper3__link">
+                <span className="link-underlined">
+                  Don’t have account yet?{" "}
+                </span>
+                <span className="link-colored">Register</span>
+              </Link>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
